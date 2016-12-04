8418d7354f737008947f14d2315655531c1bc097
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-04 00:53:33 +0000

    delete old files

--- blocks/debugmsgsender.js
+++ blocks/debugmsgsender.js
@@ -1,11 +1,10 @@
 var PubNub = require('pubnub')
 
 var pubnub = new PubNub({
-    subscribeKey: "sub-c-6d62ed1e-b978-11e6-b490-02ee2ddab7fe",
-    publishKey: "pub-c-04cd03f7-3663-49f9-86dd-c51f7f52dd57",
+    subscribeKey: "api keys",
+    publishKey: "api keys",
     ssl: true
 });
-
 
 pubnub.publish(
     {
