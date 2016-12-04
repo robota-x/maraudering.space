5c30d91acaf0c69a22fe03ddc52dbc623f5afcb4
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-04 01:23:52 +0000

    add centering, tweak zooming and var names

--- website/mapScript.js
+++ website/mapScript.js
@@ -1,11 +1,20 @@
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+
+  var mapCenter = {lat: 51.544360, lng: -0.020020};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+    zoom: 15,
+    center: mapCenter
   });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
+
+  var playerMarkers = {};
+
+  centerMapOnPlayer(map);
+}
+
+function centerMapOnPlayer(map) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      map.setCenter({lat: pos.coords.latitude, lng: pos.coords.longitude});
+    });
+  }
 }
