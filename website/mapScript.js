108ac7ab223a42025047f9612e5016d1d2b07725
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-04 01:34:10 +0000

    add marking function

--- website/mapScript.js
+++ website/mapScript.js
@@ -1,14 +1,14 @@
-function initMap() {
+var playerMarkers = {};  // global var? what could possibly go wrong?
 
+function initMap() {
   var mapCenter = {lat: 51.544360, lng: -0.020020};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: mapCenter
   });
 
-  var playerMarkers = {};
-
   centerMapOnPlayer(map);
+  setMarker(mapCenter, map);
 }
 
 function centerMapOnPlayer(map) {
@@ -16,5 +16,17 @@ function centerMapOnPlayer(map) {
     navigator.geolocation.getCurrentPosition(function(pos) {
       map.setCenter({lat: pos.coords.latitude, lng: pos.coords.longitude});
     });
+  }
+}
+
+function setMarker(mark, map) {
+  if (mark.id in playerMarkers) {
+
+  } else {
+    playerMarkers[mark.id] =
+      new google.maps.Marker({
+        position: {lat: mark.lat, lng: mark.lng},
+        map: map
+      });
   }
 }
