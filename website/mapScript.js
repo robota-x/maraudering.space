a3927514b0caf5d3e9d629b87d3e52428c071de9
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-12-04 01:56:22 +0000

    implement mark setting from pubnub subscription

--- website/mapScript.js
+++ website/mapScript.js
@@ -1,17 +1,17 @@
 var playerMarkers = {};  // global var? what could possibly go wrong?
-
+var map;
 function initMap() {
   var mapCenter = {lat: 51.544360, lng: -0.020020};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: mapCenter
   });
 
-  centerMapOnPlayer(map);
-  setMarker(mapCenter, map);
+  centerMapOnPlayer();
+  // setMarker(mapCenter);
 }
 
-function centerMapOnPlayer(map) {
+function centerMapOnPlayer() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(pos) {
       map.setCenter({lat: pos.coords.latitude, lng: pos.coords.longitude});
@@ -19,13 +19,14 @@ function centerMapOnPlayer(map) {
   }
 }
 
-function setMarker(mark, map) {
+function setMarker(mark) {
+  // console.log('marker set', mark);
   if (mark.id in playerMarkers) {
 
   } else {
-    playerMarkers[mark.id] =
+    playerMarkers[mark.playerID] =
       new google.maps.Marker({
-        position: {lat: mark.lat, lng: mark.lng},
+        position: {lat: mark.latitude, lng: mark.longitude},
         map: map
       });
   }
@@ -34,13 +35,15 @@ function setMarker(mark, map) {
 // pubnub polling and more global polluting.
 
 pubnub = PUBNUB({
-        publish_key : 'demo',
-        subscribe_key : 'demo'
+        subscribe_key : 'sub-c-6d62ed1e-b978-11e6-b490-02ee2ddab7fe'
     })
 
 pubnub.subscribe({
     channel : "location",
     message : function (message) {
-        console.log('message', message);
+      console.log('got message', message);
+        if (message.latitude && message.longitude && message.playerID) {    // some quick validation to avoid nuking the map with bad data
+          setMarker(message);
+        }
     },
 })
